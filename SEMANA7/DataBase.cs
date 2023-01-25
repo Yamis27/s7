a55f4648df4b6cf54797b5b40b09a41805a8c30f
user <user@DESKTOP-K5OULKS>
2023-01-24 20:04:47 -0500

    Agregar archivos de proyecto.

---
+++ SEMANA7/DataBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace SEMANA7
+{
+    public interface DataBase
+    {
+        SQLiteAsyncConnection GetConnection();
+
+    }
+}
---
+++ SEMANA7/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SQLite;
+
+namespace SEMANA7
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
